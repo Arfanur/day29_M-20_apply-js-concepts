115a0d9aa5d8e170c8ae2dcbe126ddfce1bb90fa
Arfanur Rahman <arfanur03@gmail.com>
2022-02-10 17:56:04 +0600

    6_factorial-function

--- 6_factorial-function.js
+++ 6_factorial-function.js
@@ -1,5 +1,20 @@
-let factorial = 1;
-for (let i = 1; i <= 5; i++) {
-    factorial = factorial *i;
-    console.log(factorial);
+// let factorial6th = 1;
+// console.log('5!');
+// for (let i = 1; i <= 5; i++) {
+//     factorial6th = factorial6th *i;
+// }
+// console.log(factorial6th);
+
+function getFactorial(number) {
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+        factorial = factorial * i;
+    }
+    return factorial;
 }
+var firstFactorial = getFactorial(7);
+console.log('Factorial of 7 is = ', firstFactorial);
+
+var secondFactorial = getFactorial(9);
+console.log('Factorial of 9 is = ', secondFactorial);
+
