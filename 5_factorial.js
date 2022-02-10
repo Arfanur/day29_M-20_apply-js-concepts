7b06dcf63dbeb7186b657b1b93430fc0557ba2ab
Arfanur Rahman <arfanur03@gmail.com>
2022-02-10 17:28:15 +0600

    5.1_factorial

--- 5_factorial.js
+++ 5_factorial.js
@@ -11,6 +11,6 @@ var factorial = 1;
 console.log('7!');
 for (var i = 1; i <= 7; i++) {
     factorial = factorial * i;
-    console.log(factorial);
 }
+console.log(factorial);
 // console.log(factorial);
